731683b401c2448010b5d8daea3f7083c1eafd67
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-08 17:53:15 -0500

    Using Null

--- Self_Join.sql
+++ Self_Join.sql
@@ -5,6 +5,10 @@ SELECT COUNT(name) FROM stops
 SELECT id FROM stops
 WHERE name = 'Craiglockhart'
 
+/* 3.-Give the id and the name for the stops on the '4' 'LRT' service. */
+SELECT id, name FROM stops JOIN route ON stops.id = route.stop
+WHERE num = '4' AND company = 'LRT';
+
 
 /* 4.-The query shown gives the number of routes that visit either London Road (149) or Craiglockhart (53). Run the query and notice the two services that link these stops have a count of 2. Add a HAVING clause to restrict the output to these two routes. */
   SELECT company, num, COUNT(*)
@@ -25,3 +29,32 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
+
+/* 7.- Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith') */
+SELECT DISTINCT R1.company, R1.num
+FROM route R1, route R2
+WHERE R1.num=R2.num AND R1.company=R2.company
+AND R1.stop=115 AND R2.stop=137
+
+/* 8.-Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross' */
+
+SELECT R1.company, R1.num
+FROM route R1, route R2, stops S1, stops S2
+WHERE R1.num=R2.num AND R1.company=R2.company
+AND R1.stop=S1.id AND R2.stop=S2.id
+AND S1.name='Craiglockhart'
+AND S2.name='Tollcross'
+
+/* 9.- Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
+ */
+
+ SELECT DISTINCT S2.name, R2.company, R2.num
+FROM stops S1, stops S2, route R1, route R2
+WHERE S1.name='Craiglockhart'
+AND S1.id=R1.stop
+AND R1.company=R2.company AND R1.num=R2.num
+AND R2.stop=S2.id
+
+/* 10.- Find the routes involving two buses that can go from Craiglockhart to Lochend.
+Show the bus no. and company for the first bus, the name of the stop for the transfer,
+and the bus no. and company for the second bus. */
