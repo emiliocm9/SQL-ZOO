3dab519baceb0f0d1b5485f8ff312c05cd41d0c7
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-08 15:58:17 -0500

    Using Null

--- Using_NULL.sql
+++ Using_NULL.sql
@@ -31,3 +31,4 @@ SELECT name, (CASE WHEN dept IN (1, 2) THEN 'Sci' ELSE 'Art' END) AS name FROM t
 
 /* 10.- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise. */
 SELECT name, (CASE WHEN dept IN (1, 2) THEN 'Sci' WHEN dept = 3 THEN 'Art' ELSE 'None' END) AS name FROM teacher
+
